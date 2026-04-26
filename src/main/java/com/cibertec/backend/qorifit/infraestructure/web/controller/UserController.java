7098e3c4566a1d81449d27d35b58122e47927de2
LifoX404 <ccorrales.dev@outlook.com>
2026-04-26 14:48:26 -0500

    fix cors

--- src/main/java/com/cibertec/backend/qorifit/infraestructure/web/controller/UserController.java
+++ src/main/java/com/cibertec/backend/qorifit/infraestructure/web/controller/UserController.java
@@ -6,6 +6,7 @@ import com.cibertec.backend.qorifit.infraestructure.web.dto.response.UserDto;
 import com.cibertec.backend.qorifit.utils.InternalCodes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class UserController {
 
     private final UserService userService;
