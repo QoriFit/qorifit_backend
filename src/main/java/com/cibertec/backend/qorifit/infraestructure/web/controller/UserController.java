4086ef759031d7e9bded70dc2820bb5bc2c8535d
LifoX404 <ccorrales.dev@outlook.com>
2026-04-26 15:12:37 -0500

    XD

--- src/main/java/com/cibertec/backend/qorifit/infraestructure/web/controller/UserController.java
+++ src/main/java/com/cibertec/backend/qorifit/infraestructure/web/controller/UserController.java
@@ -7,6 +7,7 @@ import com.cibertec.backend.qorifit.utils.InternalCodes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,7 @@ public class UserController {
 
     private final UserService userService;
 
+    @GetMapping
     public ResponseEntity<ApiResponse<?>> getUsers(){
 
         List<UserDto> response = userService.getUsers();
